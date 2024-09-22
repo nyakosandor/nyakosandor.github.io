f277e9af6eabeb7dd2cc9af2e8cefd316d7fd740
Sandor Nyako <148873777+nyakosandor@users.noreply.github.com>
2024-09-22 13:39:03 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function toggleDetails(memberId) {
+    const details = document.getElementById(memberId);
+    if (details.style.display === "none" || details.style.display === "") {
+        details.style.display = "block";
+    } else {
+        details.style.display = "none";
+    }
+}
+
